ff2ef31fe32705f45b3fbbdb28f4c4f747722a70
Kevin Dallian <kevin.dallian@binus.ac.id>
2023-03-21 17:15:28 +0700

    Initial Commit

---
+++ StudySessionScheduler/StudySessionSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySessionSchedulerApp.swift
+//  StudySessionScheduler
+//
+//  Created by Kevin Dallian on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct StudySessionSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
