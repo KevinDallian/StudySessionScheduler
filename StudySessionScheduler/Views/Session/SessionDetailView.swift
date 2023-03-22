a1c4c750d79fce4d9c6ca3064f7bdd2ab5d109a5
grisvian <grisvian.verrel@gmail.com>
2023-03-22 23:00:46 +0700

    Session detail dari sati room

---
+++ StudySessionScheduler/Views/Session/SessionDetailView.swift
@@ -0,0 +1,68 @@
+//
+//  SessionDetailView.swift
+//  StudySessionScheduler
+//
+//  Created by Grisvian Verrel on 22/03/23.
+//
+
+import SwiftUI
+
+struct SessionDetailView: View {
+    @State var name:String = ""
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.blue)
+                .ignoresSafeArea()
+            RoundedRectangle(cornerRadius: 34)
+                .offset(y: 125)
+                .foregroundColor(.white)
+            VStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 154, height: 154)
+                Text("Swift Class")
+                    .font(.largeTitle.weight(.bold))
+                Text("By KD")
+                VStack(alignment: .leading) {
+                    Text("Date Time")
+                        .font(.subheadline.weight(.bold))
+                    HStack {
+                        Text ("Mar 5, 2023")
+                        Spacer ()
+                        Text ("6:00 PM")
+                    }
+                    .padding(.bottom)
+                    Text("At")
+                        .font(.subheadline.weight(.bold))
+                    Text ("Lab - 01")
+                        .padding(.bottom)
+                    Text("Description")
+                        .font(.subheadline.weight(.bold))
+                    Text  ("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.")
+                        .padding(.bottom)
+                    Text("Your Name")
+                        .font(.subheadline.weight(.bold))
+                    
+                    TextField ("name", text: $name)
+                        .cornerRadius(10)
+                        .border(.secondary)
+                }.padding([.leading, .bottom, .trailing])
+                Button ("Join Session")
+                {
+                    
+                } .padding(.top)
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+            }
+        }
+    }
+}
+
+struct SessionDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SessionDetailView()
+    }
+}
