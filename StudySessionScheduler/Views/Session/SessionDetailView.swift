8f464dfb8a972580a994c2964fc3516902499dbe
grisvian <grisvian.verrel@gmail.com>
2023-03-24 14:44:13 +0700

    adding navigation view

--- StudySessionScheduler/Views/Session/SessionDetailView.swift
+++ StudySessionScheduler/Views/Session/SessionDetailView.swift
@@ -9,55 +9,75 @@ import SwiftUI
 
 struct SessionDetailView: View {
     @State var name:String = ""
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 10)
-                .foregroundColor(.blue)
-                .ignoresSafeArea()
-            RoundedRectangle(cornerRadius: 34)
-                .offset(y: 125)
-                .foregroundColor(.white)
-            VStack {
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 154, height: 154)
-                Text("Swift Class")
-                    .font(.largeTitle.weight(.bold))
-                Text("By KD")
-                VStack(alignment: .leading) {
-                    Text("Date Time")
-                        .font(.subheadline.weight(.bold))
-                    HStack {
-                        Text ("Mar 5, 2023")
-                        Spacer ()
-                        Text ("6:00 PM")
-                    }
-                    .padding(.bottom)
-                    Text("At")
-                        .font(.subheadline.weight(.bold))
-                    Text ("Lab - 01")
+        NavigationView{
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.blue)
+                    .ignoresSafeArea()
+                RoundedRectangle(cornerRadius: 34)
+                    .offset(y: 125)
+                    .foregroundColor(.white)
+                VStack {
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .background(.white)
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(width: 154, height: 154)
+                    Text("Swift Class")
+                        .font(.largeTitle.weight(.bold))
+                    Text("By KD")
+                    VStack(alignment: .leading) {
+                        Text("Date Time")
+                            .font(.subheadline.weight(.bold))
+                        HStack {
+                            Text ("Mar 5, 2023")
+                            Spacer ()
+                            Text ("6:00 PM")
+                        }
                         .padding(.bottom)
-                    Text("Description")
-                        .font(.subheadline.weight(.bold))
-                    Text  ("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.")
-                        .padding(.bottom)
-                    Text("Your Name")
-                        .font(.subheadline.weight(.bold))
+                        Text("At")
+                            .font(.subheadline.weight(.bold))
+                        Text ("Lab - 01")
+                            .padding(.bottom)
+                        Text("Description")
+                            .font(.subheadline.weight(.bold))
+                        Text  ("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.")
+                            .padding(.bottom)
+                        Text("Your Name")
+                            .font(.subheadline.weight(.bold))
+                        
+                        TextField ("name", text: $name)
+                            .cornerRadius(10)
+                            .border(.secondary)
+                    }.padding([.leading, .bottom, .trailing])
+                    Button ("Join Session")
+                    {
+                        
+                    } .padding(.top)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.green)
                     
-                    TextField ("name", text: $name)
-                        .cornerRadius(10)
-                        .border(.secondary)
-                }.padding([.leading, .bottom, .trailing])
-                Button ("Join Session")
-                {
+                }
+            }.toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button(action : { self.presentationMode.wrappedValue.dismiss() }){
+                        Image(systemName: "arrowshape.turn.up.backward.circle")
+                            .font(.title)
+                            .accentColor(.white)
+                    }.navigationBarBackButtonHidden(true)
+                    Text("Back")
+                        .font(.title.weight(.bold))
+                        .foregroundColor(.white)
                     
-                } .padding(.top)
-                .buttonStyle(.borderedProminent)
-                .tint(.green)
+                }
             }
         }
+        
+   
     }
 }
 
